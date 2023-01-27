991243cd637a71d29ee0580fa0db2c7b058d21af
orakka90 <orakka90@gmail.com>
2023-01-27 17:37:48 +0200

    added styles to articles

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,20 +1,17 @@
 import React, { Component } from 'react'
 import styles from '../style.css'
-import { websiteConfig } from '../config/config'
+import logo from './images/icon-modified.png'; // with import
 
 
 export default function Content(props) {
     const city = props.city
     const citiesLength = props.citiesLength
-    const mystyle = {
-        fontFamily: "'Montserrat', sans-serif; "
-    };
     let img1 = props.city.images ? props.city.images[0] : ""
     let img2 = props.city.images ? props.city.images[1] : ""
     return (
         <>
             <div className="column middle">
-                <h2>{city?.title}</h2>
+                <img className='logo' style={{ width: '3vw', height: '6vh', alignItems: 'normal' }} src={logo} />
                 <hr />
                 <h4>{city?.sub_title}</h4>
                 <hr />
@@ -22,21 +19,31 @@ export default function Content(props) {
                     <img style={{ width: '20vw', height: '40vh' }} src={(img1)} />
                     <img style={{ width: '20vw', height: '40vh' }} src={(img2)} />
                 </div>
-                <p style={mystyle}>{city?.content}</p>
                 <div className='grid_btn'>
                     {
                         city.id !== 0 ?
-                            <button onClick={() => props.prevPage()} className="btn_next"> Prev</button>
+                            <button onClick={() => props.prevPage()} className="btn_prev"> Prev</button>
                             : <></>
                     }
                     {
                         city.id !== citiesLength - 1 ?
-
                             <button onClick={() => props.nextPage()} className="btn_next">Next</button> : <></>
                     }
 
                 </div>
+                <p className="content">{city?.content}</p>
+                <div className='grid_btn'>
+                    {
+                        city.id !== 0 ?
+                            <button onClick={() => props.prevPage()} className="btn_prev"> Prev</button>
+                            : <></>
+                    }
+                    {
+                        city.id !== citiesLength - 1 ?
+                            <button onClick={() => props.nextPage()} className="btn_next">Next</button> : <></>
+                    }
 
+                </div>
             </div>
         </>
     )
--- client/src/components/RightAdd.jsx
+++ client/src/components/RightAdd.jsx
@@ -4,10 +4,7 @@ import styles from '../style.css'
 export default function RightAdd() {
     return (
         <>
-
             <div className="column side">
-                <h2></h2>
-                <p></p>
             </div>
         </>
     )
